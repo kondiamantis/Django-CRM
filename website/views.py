1e5ed17b500c71b6106ea596953d442f07e9978c
kondiamantis <kondiamantis@gmail.com>
2023-11-24 16:25:58 +0200

    Showing data from db in a table

--- website/views.py
+++ website/views.py
@@ -1,10 +1,13 @@
-import re
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 def home(request):
+
+    records = Record.objects.all()
 
     #Check to see if logging in
     if request.method == "POST":
@@ -21,7 +24,7 @@ def home(request):
             messages.success(request, 'There was an error logging in. Please try again...')
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records' : records})
 
 
 def logout_user(request):
@@ -30,4 +33,31 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html', {})
+
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            #Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have successfully registered! Welcome!!!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+        
+    return render(request, 'register.html', {'form':form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        #Look up Records
+        customer_record= Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, "You must be logged in to view that page!")
+        return redirect('home')
